e10c1ac180f7c9e78de6eb55173fc646c7669508
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-07 15:00:51 +0200

    add test script

---
+++ palindrome/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+trap "kill 0" SIGINT
+
+
+if [[ "$#" < 1 ]]; then
+    echo "Usage: ./test.sh cmd" >&2
+    exit 1
+fi
+
+diff <(cat cases.txt | eval "(time $@)") palindromes.txt
