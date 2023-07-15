59315c422bb5f85ab5377caf187571da08133f9f
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-15 10:11:51 +0200

    update

--- palindrome/cpalindromemodule.c
+++ palindrome/cpalindromemodule.c
@@ -2,6 +2,26 @@
 #include <Python.h>
 
 static PyObject *cpalindrome_is_palindrome(PyObject *self, PyObject *args) {
+    const char *phrase;
+    int l = 0;
+
+    if (!PyArg_ParseTuple(args, "s", &phrase))
+        return NULL;
+
+    int h = strlen(phrase) -1;
+
+    if (phrase[h] == '\n')
+        h--;
+
+    while (l < h) {
+        while (!isalnum(phrase[l] && l < h))
+            l++;
+        while (!isalnum(phrase[h] && l < h))
+            h--;
+        if (tolower(phrase[l++] != tolower(phrase[h--])))
+            return Py_False;
+    }
+    return Py_True;
 }
 
 static PyMethodDef CVarintMethods[] = {
