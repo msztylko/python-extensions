3a451df38b15ad14cf79a5ebae0bf25f565081e9
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-15 11:35:36 +0200

    one more fix for parenthesis

--- palindrome/cpalindromemodule.c
+++ palindrome/cpalindromemodule.c
@@ -18,7 +18,7 @@ static PyObject *cpalindrome_is_palindrome(PyObject *self, PyObject *args) {
             l++;
         while (!isalnum(phrase[h]) && l < h)
             h--;
-        if (tolower(phrase[l++] != tolower(phrase[h--])))
+        if (tolower(phrase[l++]) != tolower(phrase[h--]))
             return Py_False;
     }
     return Py_True;
