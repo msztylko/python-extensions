eca2c8d20e1eae85780fa7dbfe62a83c2a3e76bb
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-18 10:21:12 +0200

    change palindrome algo

--- palindrome/py_palindrome.py
+++ palindrome/py_palindrome.py
@@ -3,12 +3,22 @@ import sys
 
 
 def is_palindrome(phrase):
-    # remove whitespace
-    pattern = re.compile(r'\W')
-    phrase = re.sub(pattern, '', phrase)
-    # all to lowercase
-    phrase = ''.join(char.lower() for char in phrase)
-    return phrase == phrase[::-1]
+    left = 0
+    right = len(phrase) - 1
+    
+    if phrase[right] == '\n':
+        right -= 1
+
+    while left < right:
+        while (not phrase[left].isalnum() and left < right):
+            left += 1
+        while (not phrase[right].isalnum() and left < right):
+            right -= 1
+        if phrase[left].lower() != phrase[right].lower():
+            return False
+        left += 1
+        right -= 1
+    return True
 
 
 if __name__ == "__main__":
