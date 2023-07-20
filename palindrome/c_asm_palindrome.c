fef36ccaf1d49fb42917de3d663353d6d376e0cc
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-20 10:39:37 +0200

    next steps towards assembly

--- palindrome/c_asm_palindrome.c
+++ palindrome/c_asm_palindrome.c
@@ -1,6 +1,3 @@
-#include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,33 +7,55 @@ int ispalindrome(char *s, int linelen) {
     int l = 0;
     int h = linelen - 1;
 
+    char scase = 0x00;
+    char sl, sh;
+
     while (l < h) {
-        while (!(('A' <= s[l] && s[l] <= 'Z') || ('a' <= s[l] && s[l] <= 'z')) && l<h)
+        sl = s[l];
+        sh = s[h];
+        if ('a' <= sl && sl <= 'z')
+            scase |= (1 << 0);
+        if ('A' <= sl && sl <= 'Z')
+            scase |= (1 << 1);
+        if ('a' <= sh && sh <= 'z')
+            scase |= (1 << 2);
+        if ('A' <= sh && sh <= 'Z')
+            scase |= (1 << 3);
+
+        while (!(scase & 0x03) && l<h) {
             l++;
-        while (!(('A' <= s[h] && s[h] <= 'Z') || ('a' <= s[h] && s[h] <= 'z')) && l<h)
+            sl = s[l];
+            if ('a' <= sl && sl <= 'z')
+                scase |= (1 << 0);
+            if ('A' <= sl && sl <= 'Z')
+                scase |= (1 << 1);
+        }
+        while (!(scase & 0x0C) && l<h) {
             h--;
+            sh = s[h];
+            if ('a' <= sh && sh <= 'z')
+                scase |= (1 << 2);
+            if ('A' <= sh && sh <= 'Z')
+                scase |= (1 << 3);
+        }
+    
         // both uppper
-        if (('A' <= s[l] && s[l] <= 'Z') && ('A' <= s[h] && s[h] <= 'Z')) {
-            if (s[l] != s[h])
-                return 0;
-        }
+        if ((scase == 0x0A) && (sl != sh))
+            return 0;
+
         // first upper, second lower
-        if (('A' <= s[l] && s[l] <= 'Z') && ('a' <= s[h] && s[h] <= 'z')) {
-            if (s[l] != (s[h] - 32))
-                return 0;
-        }
+        if ((scase == 0x06) && (sl != sh-32))
+            return 0;
+
         // first lower, second upper
-        if (('a' <= s[l] && s[l] <= 'z') && ('A' <= s[h] && s[h] <= 'Z')) {
-            if ((s[l] - 32) != s[h])
-                return 0;
-        }
+        if ((scase == 0x09) && (sl-32 != sh))
+            return 0;
         // both lower
-        if (('a' <= s[l] && s[l] <= 'z') && ('a' <= s[h] && s[h] <= 'z')) {
-            if (s[l] != s[h])
-                return 0;
-        }
+        if ((scase == 0x05) && (sl != sh))
+            return 0;
         h--;
         l++;
+        scase = 0x00;
     }
     return 1;
 }
