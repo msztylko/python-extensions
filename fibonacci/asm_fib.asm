c34c9d8e30418ef0434d388afd5c54865868b841
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-23 10:58:58 +0200

    more dircet translation of assembly

--- fibonacci/asm_fib.asm
+++ fibonacci/asm_fib.asm
@@ -2,21 +2,24 @@ section .text
 global fib
 
 fib:
+    ; if (n < 2) return n
     mov eax, edi
-    cmp eax, 1
-    jle .end
+    cmp eax, 2
+    jl .end
 
+    ; fib(n-1)
     sub edi, 1
     push rdi
     call fib
     pop rdi
 
+    ; fib(n-2)
     sub edi, 1
     push rax
     call fib
     pop rdx
-
-    add eax, edx
+    
+    add eax, edx ; fib(n-1) + fib(n-2)
 
 .end:
     ret
