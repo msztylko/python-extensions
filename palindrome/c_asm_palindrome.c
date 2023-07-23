7bdbc3babb747491469f05f65a219d60393b8e2c
Marcin Sztylko <marcin.sztylko@gmail.com>
2023-07-23 15:10:06 +0200

    add C driver for assembly

---
+++ palindrome/c_asm_palindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char ispalindrome(char *s, char linelen);
+
+int main() {
+  size_t len;
+  ssize_t read;
+  char *line = NULL;
+  while ((read = getline(&line, &len, stdin)) != -1) {
+    
+    if (ispalindrome(line, read))
+      printf("%s", line);
+  }
+
+  if (ferror(stdin))
+    fprintf(stderr, "Error reading from stdin");
+
+  free(line);
+  fprintf(stderr, "ok\n");
+}
